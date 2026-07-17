705e5fcbd71119b6baad7de845fa4a15f9527039
vqzpzvdbbiotechvtenure76-hub <vqzpzvdbbiotechvtenure76@gmail.com>
2026-07-18 02:23:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// 3 seconds baad popup page par redirect
+window.addEventListener("load", function () {
+    setTimeout(function () {
+        window.location.href = "pop.html";
+    }, 6000); // 3 seconds
+});
 
+window.addEventListener("load", function () {
+    setTimeout(function () {
+        document.getElementById("warningPopup").classList.add("show");
+    }, 3000);
+});
